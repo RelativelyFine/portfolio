8760dd38844d44b5bf1cf6a3e6df28b5613b5d48
David <53689532+RelativelyFine@users.noreply.github.com>
2022-05-31 03:20:56 -0400

    Check

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import reportWebVitals from './reportWebVitals'
 import { HashRouter } from 'react-router-dom'
+
 import App from './App'
 import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <App />
     </HashRouter>
     ,
