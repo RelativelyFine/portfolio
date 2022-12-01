023734d0c397ed1d906261346802aa6b6ba681f4
David <53689532+RelativelyFine@users.noreply.github.com>
2022-12-01 18:51:50 +0000

    Directory Fix

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/portfolio" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
+          <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/dashboard" element={<Dashboard />} />
